0c0ea3a7edc70b0050970bea68dff1e211f44779
lecture-bisper <bisper@outlook.kr>
2022-11-09 16:46:06 +0900

    맵퍼 파일 생성.

---
+++ src/main/java/com/bitc/board/mapper/BoardMapper.java
@@ -0,0 +1,12 @@
+package com.bitc.board.mapper;
+
+import com.bitc.board.dto.BoardDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+// @Mapper : mybatis orm을 사용하여 xml파일과 연동된 인터페이스임을 알려주는 어노테이션
+@Mapper
+public interface BoardMapper {
+  List<BoardDto> selectBoardList() throws Exception;
+}
